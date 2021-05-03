1b63f5ccfd70d108c64f6ef82a6db289a5164fb0
ckacldla12 <74747290+ckacldla12@users.noreply.github.com>
2021-05-03 11:15:35 +0900

    Add files via upload
    2차수정

--- pikle.py
+++ pikle.py
@@ -1,7 +1,11 @@
 import pickle
-
+# 202025736 신동원 파일실행 될 시 파일을 불러오고 파일 저장을 누르면 저장이되고 종료됨
 def main():
-   address_book ={} 				# 공백 딕셔너리를 생성한다.
+   print("파일을 불러왔습니다")
+   with open("./addressData.bin", "rb") as f:
+       address_book = pickle.load(f)
+       print(address_book)
+
    while True:
        user = display_menu();
        info = []
@@ -30,12 +34,7 @@ def main():
            pickle.dump(address_book, f)
            f.close()
            print("파일이 저장되었습니다!")
-       elif user == 6:  # 파일 불러오기
-           with open("./addressData.bin", "rb") as f:
-               address_book = pickle.load(f)
-               print(address_book)
-       else:
-          break
+           break
 
 # 이름과 전화번호를 입력받아서 반환한다.
 def get_contact():
@@ -55,8 +54,6 @@ def display_menu() :
    print("3. 연락처 검색")
    print("4. 연락처 출력")
    print("5. 연락처 파일 저장")
-   print("6. 파일 불러 오기")
-   print("7. 종료")
    select = int(input("메뉴 항목을 선택하시오: "))
    return select
 
